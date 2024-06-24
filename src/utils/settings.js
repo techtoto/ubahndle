5ca12e2b2d87d1ca540af98ef7ffbbd57e8b2634
techtoto <toto.rei0210@gmail.com>
2024-06-24 22:37:31 +0200

    add auto dark mode
    (cherry picked from commit e931c6c0aebfae8c52919d21ba642811ef480a18)

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -3,10 +3,12 @@ import {
   saveSettingsToLocalStorage,
 } from './localStorage'
 
+const { matches: darkMode } = window.matchMedia("(prefers-color-scheme: dark)")
+
 export const defaultSettings = {
   display: {
     showAnswerStatusBadges: false,
-    darkMode: false,
+    darkMode,
   }
 }
 
