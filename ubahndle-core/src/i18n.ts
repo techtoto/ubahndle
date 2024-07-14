ee117ec5768fe38581e8c74a66c21891addd3ec1
Marie Ramlow <me@nycode.dev>
2024-07-14 15:50:52 +0200

    move frankfurt specific translations out of core

--- ubahndle-core/src/i18n.ts
+++ ubahndle-core/src/i18n.ts
@@ -4,19 +4,31 @@ import { initReactI18next } from 'react-i18next'
 import german from "../translations/de.json" assert { type: "json" }
 import english from "../translations/en.json" assert { type: "json" }
 
-export function initI18n() {
+// languages we support currently
+type Languages = "de" | "en";
+type Translations = Record<Languages, any>;
+
+export function initI18n(brandTranslations: Translations) {
   return i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
       debug: true,
       fallbackLng: 'en',
+      ns: ["core"],
+      defaultNS: "core",
       interpolation: {
         escapeValue: false, // not needed for react as it escapes by default
       },
       resources: {
-        de: german,
-        en: english,
+        de: {
+          core: german,
+          brand: brandTranslations.de,
+        },
+        en: {
+          core: english,
+          brand: brandTranslations.en,
+        },
       }
     })
 }
